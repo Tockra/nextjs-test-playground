217ef763e425eac3c6932caeceb8663e0714ef5f
Tim <Tockra@users.noreply.github.com>
2025-01-17 09:53:22 +0100

    - upgrade to version 15

--- src/app/api/test/route.tsx
+++ src/app/api/test/route.tsx
@@ -2,9 +2,8 @@ import { Test } from "@/components/test";
 import { compile } from "@onedoc/react-print";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, unstable_setRequestLocale } from "next-intl/server";
-import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: Request) {
   unstable_setRequestLocale("de");
   const messages = await getMessages({ locale: "de" }); // without locale:de this don't work
   const htmlString = await compile(<>
